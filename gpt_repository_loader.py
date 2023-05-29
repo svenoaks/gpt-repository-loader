d5b92db083a24f5bfc6b1c755423e9577c792ae8
steve <svenoaks@gmail.com>
2023-05-29 09:57:44 -0400

    added --input-files and --splits flags

--- gpt_repository_loader.py
+++ gpt_repository_loader.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import fnmatch
+import argparse
 
 def get_ignore_list(ignore_file_path):
     ignore_list = []
@@ -19,11 +20,17 @@ def should_ignore(file_path, ignore_list):
             return True
     return False
 
-def process_repository(repo_path, ignore_list, output_file):
+def process_repository(repo_path, ignore_list, output_file_path, input_files, splits):
+    split_count = 0
+    file_count = 0
+    output_file = open(f"{output_file_path}_{split_count}.txt", 'w')
     for root, _, files in os.walk(repo_path):
         for file in files:
             file_path = os.path.join(root, file)
             relative_file_path = os.path.relpath(file_path, repo_path)
+
+            if input_files and relative_file_path not in input_files:
+                continue
 
             if not should_ignore(relative_file_path, ignore_list):
                 with open(file_path, 'r', errors='ignore') as file:
@@ -31,13 +38,27 @@ def process_repository(repo_path, ignore_list, output_file):
                 output_file.write("-" * 4 + "\n")
                 output_file.write(f"{relative_file_path}\n")
                 output_file.write(f"{contents}\n")
+                file_count += 1
+
+                if file_count >= splits:
+                    output_file.write("--END--")
+                    output_file.close()
+                    split_count += 1
+                    output_file = open(f"{output_file_path}_{split_count}.txt", 'w')
+                    file_count = 0
+    output_file.write("--END--")
+    output_file.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python git_to_text.py /path/to/git/repository [-p /path/to/preamble.txt] [-o /path/to/output_file.txt]")
-        sys.exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("repo_path", help="/path/to/git/repository")
+    parser.add_argument("-p", "--preamble", help="/path/to/preamble.txt")
+    parser.add_argument("-o", "--output", help="/path/to/output_file.txt", default='output')
+    parser.add_argument("-i", "--input_files", nargs='*', help="List of specific files to be included in the output file.")
+    parser.add_argument("-s", "--splits", type=int, default=1, help="Number of output files.")
+    args = parser.parse_args()
 
-    repo_path = sys.argv[1]
+    repo_path = args.repo_path
     ignore_file_path = os.path.join(repo_path, ".gptignore")
     if sys.platform == "win32":
         ignore_file_path = ignore_file_path.replace("/", "\\")
@@ -47,28 +68,16 @@ if __name__ == "__main__":
         HERE = os.path.dirname(os.path.abspath(__file__))
         ignore_file_path = os.path.join(HERE, ".gptignore")
 
-    preamble_file = None
-    if "-p" in sys.argv:
-        preamble_file = sys.argv[sys.argv.index("-p") + 1]
-
-    output_file_path = 'output.txt'
-    if "-o" in sys.argv:
-        output_file_path = sys.argv[sys.argv.index("-o") + 1]
+    preamble_file = args.preamble
+    output_file_path = args.output
+    input_files = args.input_files if args.input_files else None
+    splits = args.splits if args.splits else 1
 
     if os.path.exists(ignore_file_path):
         ignore_list = get_ignore_list(ignore_file_path)
     else:
         ignore_list = []
 
-    with open(output_file_path, 'w') as output_file:
-        if preamble_file:
-            with open(preamble_file, 'r') as pf:
-                preamble_text = pf.read()
-                output_file.write(f"{preamble_text}\n")
-        else:
-            output_file.write("The following text is a Git repository with code. The structure of the text are sections that begin with ----, followed by a single line containing the file path and file name, followed by a variable amount of lines containing the file contents. The text representing the Git repository ends when the symbols --END-- are encounted. Any further text beyond --END-- are meant to be interpreted as instructions using the aforementioned Git repository as context.\n")
-        process_repository(repo_path, ignore_list, output_file)
-    with open(output_file_path, 'a') as output_file:
-        output_file.write("--END--")
-    print(f"Repository contents written to {output_file_path}.")
-    
+    process_repository(repo_path, ignore_list, output_file_path, input_files, splits)
+    print(f"Repository contents written to {output_file_path}_*.txt")
+
